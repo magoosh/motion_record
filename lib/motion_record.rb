5b54ed56029a4cfb314d02bdc60d0f33f48b5023
Zach Millman <zach.millman@gmail.com>
2015-03-13 14:39:42 -0700

    Fix NameError from file dependency order

--- lib/motion_record.rb
+++ lib/motion_record.rb
@@ -26,6 +26,7 @@ Motion::Project::App.setup do |app|
 
   # Some files don't have the same dependency order and alphabetic order
   {
+    "motion_record/persistence.rb" => "motion_record/serialization.rb",
     "motion_record/persistence.rb" => "motion_record/scope_helpers.rb"
   }.each do |file, dependency|
     app.files_dependencies File.join(dirname, file) => File.join(dirname, dependency)
